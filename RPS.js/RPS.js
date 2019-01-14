b01fc0605ea6ca69f0e4a7556ecc3cd4cf20f884
Felix Almejo <falmejo072268@gmail.com>
2019-01-13 18:21:35 -0800

    bug fix

--- RPS.js/RPS.js
+++ RPS.js/RPS.js
@@ -1,8 +1,44 @@
 
 let computerPlay=()=>{
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    let computerSelection = [Math.floor(Math.random() * choices.length)];
-    return console.log(computerSelection);
+    let computerSelection = ['rock', 'paper', 'scissors'];
+    return computerSelection[Math.floor(Math.random() * computerSelection.length)];
+    
 };
 
-computerPlay();
+let playerSelection = "rock";
+const computerSelection = computerPlay();
+
+let playRound=(playerSelection, computerSelection)=>{
+    // let caseSensitivity = playerSelection.toUpperCase(); issue with code come back to it possibly scope
+    if(playerSelection === computerSelection){
+        return "It's a tie!";
+    } 
+        else if(playerSelection == 'rock' && computerSelection == 'paper'){
+            return "You lose!";
+        } 
+        else if(playerSelection == 'rock' && computerSelection == 'scissors'){
+            return "Wohoo! You Win!";
+        } 
+        else if(playerSelection == 'paper' && computerSelection == 'rock'){
+            return "Wohoo! You Win!";
+        } 
+        else if(playerSelection == 'paper' && computerSelection == 'scissors'){
+            return "You Lose!";
+        } 
+        else if(playerSelection == 'scissors' && computerSelection == 'paper'){
+            return "Wohoo! You Win!";
+        } 
+        else if(playerSelection == 'scissors' && computerSelection == 'rock'){
+            return "You Lose!";
+        } 
+        else{ return "Invalid input, try again."};
+    
+    
+}
+
+
+
+
+console.log(playRound(playerSelection, computerSelection));
+
+
